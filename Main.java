681f384182b3ffe475bcd36d2523139dff275a56
togasawa <a.togasawa.sys22@morijyobi.ac.jp>
2022-11-08 10:26:37 +0900

    二人分測定できるように変更

--- Main.java
+++ Main.java
@@ -4,13 +4,14 @@ public class Main{
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
 		for(int i=0;i<2;i++){
+
 		System.out.print("身長を入力してください：");
 		double a = s.nextDouble();
 		System.out.print("体重を入力してください：");
 		double b = s.nextDouble();
 		a=a/100;
 		double c = b / (a*a);
-		System.out.println("あなたのBMIは"+String.format("%.2f",c)+"です");
+		System.out.println("あなたのBMIは"+String.format("%.2f",c)+"です。");
 		}
 	}
 }
