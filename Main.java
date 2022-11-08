e2789d53d20485963fcdf0f71418e9259b4920d8
togasawa <a.togasawa.sys22@morijyobi.ac.jp>
2022-11-08 10:18:34 +0900

    ñlªÌBMI𔻒èł«é悤ɕύX
    Please enter the commit message for your changes. Lines starting

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Main{
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
+		for(int i=0;i<2;i++){
 		System.out.print("身長を入力してください：");
 		double a = s.nextDouble();
 		System.out.print("体重を入力してください：");
@@ -10,5 +11,6 @@ public class Main{
 		a=a/100;
 		double c = b / (a*a);
 		System.out.println("あなたのBMIは"+String.format("%.2f",c)+"です");
+		}
 	}
 }
